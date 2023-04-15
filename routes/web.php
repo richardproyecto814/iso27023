26d0add629f1cb01ea4f6c2a49e33b0de4639078
Edwin Pacheco F <efrgau@gmail.com>
2023-04-14 21:05:06 -0500

    Commit de inicio 14 de abril

---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/inicio', function () {
+    return view('index');
+});
+
+
+
+Route::get('/dash', function () {
+    return view('login');
+});
+
+Route::get('/usuarios', function () {
+    return view('usuarios/listausuarios');
+});
+
+Route::get('/crearusuario', function () {
+    return view('usuarios/registrousuario');
+});
+
+Route::get('/menu', function () {
+    return view('layoutmenu');
+});
+Route::get('/activos', function () {
+    return view('activos/gestiondeactivos');
+});
+Route::get('/activos1', function () {
+    return view('activos/entornoempresarial');
+});
+
+Route::get('/activos2', function () {
+    return view('activos/gobernanza');
+});
+
+Route::get('/activos3', function () {
+    return view('activos/evaluacionriesgos');
+});
+
+Route::get('/activos4', function () {
+    return view('activos/parametrosiso');
+});
+
