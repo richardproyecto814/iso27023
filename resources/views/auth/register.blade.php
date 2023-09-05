05860c0564d0cc9d75c089a4097e56948f092d65
richard <richardproyecto814@gmail.com>
2023-09-05 11:26:16 -0500

    Registro de usuario y login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,21 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Registro</title>
-</head>
+@extends('layout.app-master')
+@section('content')
 <body>
-    <form action="/register" method="POST">
-        @csrf
-        Nombre: <input type="text" name="name">
-        Identificación<input type="text" name="identification">
-        Celular<input type="text" name="celular">
-        Email<input type="email" name="email">
-        Password<input type="password" name="password">
-        Password Confirmación<input type="password" name="password_confirmation">
-        <input type="submit" value="Registrar">
-    </form>
+    <div class="page-wrapper">
+        <div class="page-content--bge5">
+            <div class="container">
+                <div class="login-wrap">
+                    <div class="login-content">
+                    <div class="login-logo">
+                        <a href="#">
+                            <img src="images/icon/logo.png" alt="CoolAdmin">
+                        </a>
+                    </div>
+                    <div class="login-form">
+                        <form action="/register" method="POST">
+                            @csrf
+                            @include('layout.partials.messages')
+                            <div class="form-group">
+                                <label for="">Nombre</label> 
+                                <input type="text" name="name" class="au-input au-input--full" value="{{old('name')}}">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Identificación</label>
+                                <input type="text" name="identification" class="au-input au-input--full" value="{{old('identification')}}">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Celular</label>
+                                <input type="text" name="celular" class="au-input au-input--full"  value="{{old('celular')}}">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Email</label>
+                                <input type="email" name="email" class="au-input au-input--full"  value="{{old('email')}}">
+                            </div>    
+                            <div class="form-group">
+                                <label for="">Password</label>
+                                <input type="password" name="password" class="au-input au-input--full"  value="{{old('password')}}">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Password Confirmación</label>
+                                <input type="password" name="password_confirmation" class="au-input au-input--full"  value="{{old('password_confirmation')}}">
+                            </div>
+                            <input type="submit" value="Registrar" class="btn au-btn au-btn--block au-btn--green m-b-20">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
-</html>
+@endSection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/inicio', function () {
+Route::get('/', function () {
     return view('index');
 });
 
@@ -27,6 +28,9 @@ Route::get('/login',[LoginController::class,'show']);
 Route::post('/login',[LoginController::class,'login']);
 
 Route::get('/home',[HomeController::class,'index']);
+
+Route::get('/logout',[LogoutController::class,'logout']);
+
 
 
 Route::get('/dash', function () {
